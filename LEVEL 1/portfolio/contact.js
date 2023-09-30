588a0e86712f418829a56dd8ad7c37f2467116ed
aliza zahid <alizayzah11@gmail.com>
2023-09-30 18:20:54 +0500

    contact section with js done

--- LEVEL 1/portfolio/contact.js
+++ LEVEL 1/portfolio/contact.js
@@ -1,7 +1,24 @@
-function sendmail(){
-    let parameters={
-        name:document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        message:document.getElementById("message").value,
-    }
+function sendMail(event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_1wdavij";
+    const templateID = "template_ilgxzkf";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully!!");
+        })
+        .catch(err => console.log(err));
 }
+
+  
