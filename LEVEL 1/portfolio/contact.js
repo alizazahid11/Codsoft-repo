252ffede0c92952b2e0ea10943059490eadf7a56
aliza zahid <alizayzah11@gmail.com>
2023-09-30 17:32:37 +0500

    adding js file for sending email

---
+++ LEVEL 1/portfolio/contact.js
@@ -0,0 +1,7 @@
+function sendmail(){
+    let parameters={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+    }
+}
